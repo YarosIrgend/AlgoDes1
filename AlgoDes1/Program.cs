690e76dc24ebbd7e9c584e9843b5d2fe603e6f4e
ІП-32 Клобуков Ярослав <klobukov.yaroslav@lll.kpi.ua>
2024-09-24 01:29:02 +0300

    first

--- AlgoDes1/Program.cs
+++ AlgoDes1/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace AlgoDes1
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AlgoDes1
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal static class Program
     {
+        public static void Main(string[] args)
+        {
+            ModifiedBalancedSort.BalancedMultiwayMergeSort("entryFile10MB.txt");
+        }
+
+        private static void Generate()
+        {
+            Random random = new Random();
+            using (FileStream file = new FileStream("entryFile1GBRandom2.txt", FileMode.Create))
+            {
+                for (int j = 1; j <= 140_000_000; j++)
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes(random.Next(1, 10000000) + " ");
+                    file.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
     }
-  }
 }
